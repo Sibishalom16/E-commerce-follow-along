152c56dbe119a2ff69ae04f79d5740963e2a4a9b
Sibishalom16 <sibiraj@Sibi>
2025-01-30 14:15:37 +0530

    saved

--- backend/multer.js
+++ backend/multer.js
@@ -2,9 +2,7 @@ const multer = require('multer');
 
 // Configure multer storage
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/'); // Define your upload folder
-    },
+  destination:'../uploads',
     filename: function(req, file, cb) {
       const uniqueSuffix =  Date.now() + '-' + Math.round.apply(Math.random() * 1e9);
        // Define a unique filename
@@ -12,6 +10,6 @@ const storage = multer.diskStorage({
        cb(null,filename + "-" + uniqueSuffix + ".png"); // Define
     },
   });
- 
+
   // Initialize upload object
 exports.upload = multer({ storage: storage });
